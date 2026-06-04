7cc229d50a9bf840e1b8db04b90c2e6bd68198f5
ttolyi1 <127166343+ttolyi1@users.noreply.github.com>
2026-06-04 15:48:46 +0900

    Add files via upload

--- alarm_module.v
+++ alarm_module.v
@@ -1,25 +1,50 @@
 module alarm_module(
-    input clk,
-    input alarm_on,
-    input key_led,
-    input unlock_on,
-    output reg piezo
+input clk,
+input alarm_on,
+input key_led,
+input unlock_on,
+output reg piezo
 );
 
 reg [15:0] cnt;
+reg [3:0] beep_count;
 
 always @(posedge clk)
 begin
-    cnt <= cnt + 1;
+cnt <= cnt + 1;
 
-    if(alarm_on)
-        piezo <= cnt[7];
-    else if(unlock_on)
+```
+if(alarm_on)
+begin
+    if(beep_count < 5)
+    begin
+        if(cnt == 16'd500)
+        begin
+            cnt <= 0;
+            piezo <= ~piezo;
+
+            if(piezo == 1'b1)
+                beep_count <= beep_count + 1;
+        end
+    end
+    else
+    begin
+        piezo <= 1'b0;
+    end
+end
+else
+begin
+    beep_count <= 0;
+
+    if(unlock_on)
         piezo <= cnt[9];
     else if(key_led)
         piezo <= cnt[11];
     else
         piezo <= 1'b0;
+end
+```
+
 end
 
 endmodule
